adc6d6e00ad4193327185b6e54bdeed5def2907d
PrachiSharmaPS <prachisharma.ps01@gmail.com>
2022-12-26 10:05:59 +0530

    modify

--- src/controller/cardController.js
+++ src/controller/cardController.js
@@ -1,8 +1,11 @@
-const cardmodel = require('../model/cardModel')
-const customerModel = require('../model/customerModel')
+const cardmodel = require('../models/cardModel')
+const customerModel = require('../models/customerModel')
 var uuid = require('uuid-random')
 
 const {ValidName } = require("../validator/validation")
+
+
+
 
 
 const createCard = async function (req,res){
@@ -15,7 +18,8 @@ const createCard = async function (req,res){
 
         if (Object.keys(data).length == 0) { return  res.status(400).send({status : false, message : " Please Provide Some data"})}
 
-       
+        const cardCount = await cardmodel.find()
+        data.cardNumber = "C00" + (cardCount.length + 1)
         if(!cardType){ return res.status(400).send({status : false , message : "Please Provide card type"})}
         if( cardType !="REGULAR" && cardType != "SPECIAL"){
             return res.status(400).send({status : false , message : "cardType only in REGULAR/SPECIAL"})  
@@ -50,10 +54,6 @@ const createCard = async function (req,res){
      return res.status(500).send({status : false, message : error.message})
     }
 }
-
-
-
-
 
 const getCard = async function ( req,res){
 
