4a761040f2ee2159e955665440fea759c2ebef43
AnkushRai-222 <ankushrai222@gmail.com>
2023-01-11 17:53:48 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const mongoose = require('mongoose');
-const route = require('./router/router');
+const route = require('./router/route');
 const app=express();
 
 app.use(express.json());
